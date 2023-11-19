540c1fe7f1e64d166dc49aeb55781eaf86a055d5
unknown <nasibgi@code.edu.az>
2023-11-19 11:51:13 +0400

    calculator

---
+++ Calculator/Interfaces/ICalculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Interfaces
+{
+    interface ICalculation
+    {
+        double Multiple (double x, double y);
+        double Divide (double x, double y);
+        double Add (double x, double y);
+        double Substract (double x, double y);
+
+    }
+    class Calculation : ICalculation
+    {
+        double ICalculation.Add(double x, double y)
+        {
+            return x+y;
+        }
+
+        double ICalculation.Divide(double x, double y)
+        {
+            return x/y;
+        }
+
+        double ICalculation.Multiple(double x, double y)
+        {
+            return x*y;
+        }
+
+        double ICalculation.Substract(double x, double y)
+        {
+            return x-y;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+//Task1- Calculator proqrami duzeltmek ( ancaq toplama, vurma, bolme, cixma emelleri olacaq).
+//Service layer olacaq. Service layerde calculatoru yazacaqsiz.
+//Qeyd : Calculation classi, ICalculation interface, Calculate methodunuz olacaq.
+//Bunlardan istifade edib projecti yazin.
+
+using Calculator.Interfaces;
+
+Calculation calculator = new();
+Console.WriteLine(calculator.Add(2,3));
